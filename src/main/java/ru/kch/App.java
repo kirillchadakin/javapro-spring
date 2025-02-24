73da2f2f8b5acd037105a502e78587ac7ba58a5a
Kirill Chadakin <kirill.chadakin@gmail.com>
2025-02-24 21:06:14 +0300

    Реализован main

--- src/main/java/ru/kch/App.java
+++ src/main/java/ru/kch/App.java
@@ -1,10 +1,29 @@
 package ru.kch;
 
-/**
- * Hello world!
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.kch.model.User;
+import ru.kch.service.UserService;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ApplicationContext context = new AnnotationConfigApplicationContext("ru.kch");
+        UserService userService = context.getBean(UserService.class);
+
+        // Create and save a new user
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("JohnDoe");
+        userService.createUser(user);
+
+        // Retrieve user by ID
+        System.out.println("User found: " + userService.findUserById(1L).map(Object::toString).orElse("Not found"));
+
+        // Retrieve all users
+        System.out.println("All users: " + userService.findAll());
+
+        // Delete user
+        userService.deleteUser(user);
+        System.out.println("All users after deletion: " + userService.findAll());
     }
 }
